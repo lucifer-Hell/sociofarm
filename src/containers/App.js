5ff3910e18d3423d366590fe477231f70f7eeb75
Pankaj <luciferhell58@gmail.com>
2020-12-21 11:04:58 +0530

    naming issue

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/Header/Header'
 import Main from '../components/Main/main'
 import Footer from '../components/Footer/footer'
-import Dashboard from '../components/Dashboard/d'
+import Dashboard from '../components/Dashboard/Dashboard'
 import Auth from '../components/AuthPage/auth'
 import './App.css';
  
